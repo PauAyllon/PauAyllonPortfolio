2f8a4204013ee85520d4c18a9c54be6b8f37ec1b
Crimm <pauayllonortola@gmail.com>
2024-09-04 13:11:02 +0200

    v1.1.3

--- javascript/index_background.js
+++ javascript/index_background.js
@@ -4,12 +4,12 @@ window.onload = function () {
     
     /** */
     var background_index=[
-        './multimedia/index/7.png',
-        './multimedia/index/6.png',
-        './multimedia/index/2.png',
-        './multimedia/index/3.png',
-        './multimedia/index/4.png',
-        './multimedia/index/1.png'
+        './multimedia/index/5.jpg',
+        './multimedia/index/6.jpg',
+        './multimedia/index/3.jpg',
+        './multimedia/index/2.jpg',
+        './multimedia/index/4.jpg',
+        './multimedia/index/1.jpg',
     ]  
     /** 
     var background_index=[
